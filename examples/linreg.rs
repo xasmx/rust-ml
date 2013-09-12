f5f85ac3f01219c8cc43d90067d48df3fe31a8f3
Sami Nopanen <snopanen@gmail.com>
2013-09-11 21:16:08 -0400

    Better data for linreg example.

--- examples/linreg.rs
+++ examples/linreg.rs
@@ -2,31 +2,26 @@ extern mod gnuplot;
 extern mod la;
 extern mod ml;
 
-use std::rand;
 use std::vec;
 
 use gnuplot::*;
 use la::matrix::*;
+use la::util::read_csv;
 use ml::regression::linear;
 
 fn main() {
-  let mut xdata = vec::from_elem(100, 0.0f64);
-  for i in range(0, xdata.len()) {
-    xdata[i] = 10.0 * rand::random();
-  }
-  let mut ydata = vec::from_elem(100, 0.0f64);
-  for i in range(0, ydata.len()) {
-    ydata[i] = 4.0 * rand::random();
-  }
- 
-  let x = matrix(100, 1, xdata);
-  let y = matrix(100, 1, ydata); 
-  let lr = linear::train(&x, &y, 0.05f64, 1000);
+  fn parser(s : &str) -> f64 { std::f64::from_str(s).unwrap() };
+  let data = read_csv("example_data/linreg.csv", parser);
+
+  let x = data.get_column(0);
+  let y = data.get_column(1);
+
+  let lr = linear::train(&x, &y, 0.005f64, 1000);
 
   let mut lx = vec::from_elem(2, 0.0f64);
   let mut ly = vec::from_elem(2, 0.0f64);
   for i in range(0, lx.len()) {
-    lx[i] = (i as f64) * 10.0f64;
+    lx[i] = (i as f64) * 30.0f64;
     ly[i] = lr.predict(&matrix(1, 1, ~[lx[i]]));
   }
 
